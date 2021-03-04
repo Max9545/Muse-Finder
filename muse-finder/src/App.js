a61e393e9fb4f71b2a047aa727c2baa1912e3fd4
Max Bregman <bregmanmax91@gmail.com>
2021-03-03 21:35:47 -0700

    add getCode and GetName for country finder

--- muse-finder/src/App.js
+++ muse-finder/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import CardDisplay from './CardDisplay/CardDisplay.js'
+const { getCode, getName } = require('country-list')
 
 
 function App() {
@@ -15,10 +16,12 @@ function App() {
     },[])
 
     const fetchInfo = async ()  => {
+     
       return await fetch('https://pure-hollows-05817.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.artists.get?page=3&page_size=3&country=it&apikey=0f9be22f858591d254989feff9a29844')
     }
 
   return (
+    
     <div className="App">
       <header className="App-header">
         <p>
--- muse-finder/src/CardDisplay/CardDisplay.js
+++ muse-finder/src/CardDisplay/CardDisplay.js
@@ -2,14 +2,13 @@ import {useEffect, useState} from 'react'
 import Card from '../Card/Card.js'
 
 function CardDisplay({ artists }) {
-  console.log(artists)
-  console.log(artists.message.body.artist_list[0].artist.artist_name)
-  // console.log(artists.message.body.artist_list.map(c => c.artist_name))
+// console.log(artists.message.body.artist_list[0].artist.artist_name)
+  
 
   return (  
     <>
     <p>Artists</p>
-    <p>{artists.message.body.artist_list[0].artist.artist_name}</p>
+    {/* <p>{artists.message.body.artist_list[0].artist.artist_name}</p> */}
     <p>
       {/* {artists.message.body.artist_list.map(artist => {
         <Card
