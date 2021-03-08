6a4a6f4c35f20becb308458db4b58d76f7e6cc51
Max Bregman <bregmanmax91@gmail.com>
2021-03-07 21:01:11 -0700

    Add tag line for instructions and make fonts bigger

--- muse-finder/src/App.js
+++ muse-finder/src/App.js
@@ -2,13 +2,13 @@
 import './App.css';
 
 import Header from './Header/Header.js'
-
+import countryData from './countryData';
 import { fetchTopArtists } from './apiCalls'
 import { useEffect, useState } from 'react';
 import CardDisplay from './CardDisplay/CardDisplay.js'
-// const { getCode, getName } = require('country-list')
+const { getCode, getName } = require('country-list')
 // import { WorldMap } from "react-svg-worldmap"
-import countryData from './countryData';
+
 const WorldMap = require('react-svg-worldmap').WorldMap;
 const { CountryDropdown, RegionDropdown, CountryRegionData } = require('react-country-region-selector') 
 
@@ -19,36 +19,41 @@ function App() {
     const [currentCountry, setCurrentCountry] = useState()
     const nationData = countryData
 
-    var countrySet = (event, countryName, isoCode, value, prefix, suffix) => {
+    const mapCountrySet = (event, countryName, isoCode, value, prefix, suffix) => {
       setCurrentCountry(countryName)
       fetchTopArtists(isoCode)
       .then(data => setArtists(data))
-      // .then(() => setCurrentCountry(countryName))
-      .then(() => console.log(currentCountry))
-
   };
+
+  const dropDownCountrySet = (countryName) => {
+    console.log(getCode(countryName))
+    setCurrentCountry(countryName)
+    fetchTopArtists(getCode(countryName))
+    .then(data => setArtists(data))
+  }
   
       
   return (
   
     <div className="App">
-      {/* <header className="App-header">
-      </header> */}
        <Header/>
-       {/* <CountryDropdown
+       <p className='help-tag'>Select a country by typing in or by using the drop down list or click on the map</p>
+       <CountryDropdown
+        className='drop-down'
         value={currentCountry}
-        onChange={(val) => setCurrentCountry(val)} */}
-       {/* /> */}
+        onChange={(val) => dropDownCountrySet(val)}
+       />
+       <br/>
        <WorldMap 
         color="#1AFF1A" 
         backgroundColor='#BD9DDC'
         tooltipBgColor='#4B0092'
         tooltipTextColor='#1AFF1A'
         strokeOpacity='3'
-        title="Music Of The World" 
+        // title="Music Of The World" 
         size="responsive" 
         data={nationData}
-        onClickFunction={countrySet} 
+        onClickFunction={mapCountrySet} 
        />
       <p>
         {artists && currentCountry && <CardDisplay  country={currentCountry}artists={artists}/>}
