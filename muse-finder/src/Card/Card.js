d949b0476b9b566ea757f54700415221bb6642d3
Max Bregman <bregmanmax91@gmail.com>
2021-03-07 23:00:24 -0700

    Make fetch albums for artist fetch

--- muse-finder/src/Card/Card.js
+++ muse-finder/src/Card/Card.js
@@ -1,15 +1,17 @@
 import {useEffect, useState} from 'react'
 import './Card.css'
-import { fetchSingleArtist } from '../apiCalls'
+import { fetchSingleArtist, fetchSingleArtistAlbums } from '../apiCalls'
 
 function Card({ artist }) {
 
  const getArtist = (id) => {
    console.log('id space',id)
-   fetchSingleArtist(id)
+  //  fetchSingleArtist(id)
+  //  .then(data => console.log(data))
+   fetchSingleArtistAlbums(id)
    .then(data => console.log(data))
  }
- 
+
   return(
     <section id={artist.artist.artist}  onClick={() => getArtist(artist.artist.artist_id)} >
       <p >{artist.artist.artist_name} </p> 
--- muse-finder/src/apiCalls.js
+++ muse-finder/src/apiCalls.js
@@ -11,3 +11,12 @@ export const fetchTopArtists = (country)  => {
     .then(data => data.json())
   }
 
+  export const fetchSingleArtistAlbums = (artistID) => {
+    return fetch(`https://pure-hollows-05817.herokuapp.com/https://api.musixmatch.com/ws/1.1/artist.albums.get?artist_id=${artistID}&s_release_date=desc&g_album_name=1&apikey=0f9be22f858591d254989feff9a29844`)
+    .then(data => data.json())
+  }
+
+
+
+  //artist.albums.get?artist_id=${artistID}&s_release_date=desc&g_album_name=1
+
